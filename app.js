15aa96d24ea0fc44e9410cc321bd69742a1f1a78
Figmauchun <Figmauchun123@gmail.com>
2026-05-12 10:30:21 +0500

    numbers ........./........sdfbsdf

--- app.js
+++ app.js
@@ -72,3 +72,41 @@ function YaratishUx() {
   numbers.innerHTML = card.join("");
 }
 YaratishUx();
+//
+
+let mobilestatistics = [
+  {
+    sarlavha: "01",
+    izoh: "Introduction to UI/UX Design",
+  },
+  {
+    sarlavha: "02",
+    izoh: "User Research and Personas",
+  },
+  {
+    sarlavha: "03",
+    izoh: "Wireframing and Prototyping",
+  },
+  {
+    sarlavha: "04",
+    izoh: "Visual Design and Branding",
+  },
+  {
+    sarlavha: "05",
+    izoh: "Usability Testing and Iteration",
+  },
+];
+
+function YaratishMobile() {
+  let numbers = document.querySelector(".mobile");
+  let card = mobilestatistics.map((e) => {
+    return `
+     <div class="number bg-[#fff] hover:bg-[#f5f5f5] hover:translate-y-2 cursor-pointer transition-all duration-200 ease-in-out border-collapse border-[1px] border-[#E0E0E0] rounded-[8px] p-[20px]">
+        <h4 class="text-[24px] font-bold">${e.sarlavha}</h4>
+        <span class="text-[16px] block max-w-[150px] text-[#666666]">${e.izoh}</span>
+      </div>
+    `;
+  });
+  numbers.innerHTML = card.join("");
+}
+YaratishMobile();
